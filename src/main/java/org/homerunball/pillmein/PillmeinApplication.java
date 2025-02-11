0e287daca33a3bcd402844fe12138d88be613839
Sarang Kim <155128117+ksarangee@users.noreply.github.com>
2025-02-12 00:21:47 +0900

    initial commit

---
+++ src/main/java/org/homerunball/pillmein/PillmeinApplication.java
@@ -0,0 +1,11 @@
+package org.homerunball.pillmein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillmeinApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PillmeinApplication.class, args);
+	}
+}
